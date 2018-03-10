eb49034101e2586b44f7e682b279e0e794f385d6
Unknown <athul1998balakrishnan@gmail.com>
2018-03-10 22:54:13 +0530

    Translation Optimization

--- app/src/main/java/com/example/athul/instatranslate/NewActivity.java
+++ app/src/main/java/com/example/athul/instatranslate/NewActivity.java
@@ -1,5 +1,8 @@
 package com.example.athul.instatranslate;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.AdapterView;
@@ -38,6 +41,8 @@ public class NewActivity extends AppCompatActivity {
     Spinner spinner;
     String item;
     String [] lang = {"Bulgarian","Dutch","English","German","Greek","Gujarati","Hindi","Italian","Marathi","Malayalam","Russian","Spanish",};
+    boolean connected = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,12 +55,11 @@ public class NewActivity extends AppCompatActivity {
         spinner = (Spinner)findViewById(R.id.spinner2);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,lang);
         spinner.setAdapter(adapter);
-
+        textView1.setText("Select the language to be translated to....");
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
                 String Value= spinner.getSelectedItem().toString();
-                textView1.setText(Value);
                 switch(Value){
                     case "Bulgarian": item="bg";
                         break;
@@ -93,6 +97,7 @@ public class NewActivity extends AppCompatActivity {
 
 
     public void translate(View view) {
+        textView1.setText("Your Translation will be available soon......");
         new JsonTask().execute("https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180309T152246Z.4f19a932b29b98f4.e5ce75fa97145270e537e8ecc2d3aa80ed4d1631&text="+text+"&lang=en-"+item);
     }
 
@@ -104,6 +109,7 @@ public class NewActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(String... strings) {
+
             URL url = null;
             try {
                 url = new URL(strings[0]);
@@ -149,7 +155,21 @@ public class NewActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            textView1.setText(s);
+            ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+            if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                    connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+                //we are connected to a network
+                connected = true;
+            }
+            else
+                connected = false;
+            if(connected==true) {
+                textView1.setText(s);
+            }
+            else
+            {
+                textView1.setText("Please Connect to the Internet");
+            }
         }
     }
 
